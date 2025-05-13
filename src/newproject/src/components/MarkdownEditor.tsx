7819c1d8e44b799e01b0a2a498d2e173ebfbce68
gotsui <krhamui.2016@gmail.com>
2025-05-14 00:07:39 +0900

    編集欄でタブ入力可能に変更

--- src/newproject/src/components/MarkdownEditor.tsx
+++ src/newproject/src/components/MarkdownEditor.tsx
@@ -1,6 +1,6 @@
 "use client";
 
-import React, { useState } from "react";
+import React, { useCallback, useRef, useState } from "react";
 import Markdown from "react-markdown";
 import rehypeRaw from "rehype-raw";
 import remarkGfm from "remark-gfm";
@@ -11,6 +11,31 @@ type MarkdownEditorProps = {
 };
 
 const MarkdownEditor: React.FC<MarkdownEditorProps> = ({ value, onChange }: MarkdownEditorProps) => {
+    const textareaRef = useRef<HTMLTextAreaElement>(null);
+
+    const handleKeyDown = useCallback((e: React.KeyboardEvent<HTMLTextAreaElement>) => {
+        if (e.key !== "Tab") {
+            return;
+        }
+
+        e.preventDefault();
+
+        if (!textareaRef.current) {
+            return;
+        }
+
+        const cursorPosition = textareaRef.current.selectionStart;
+
+        if (!cursorPosition) {
+            return;
+        }
+
+        const cursorLeft = textareaRef.current.value.substring(0, cursorPosition);
+        const cursorRight = textareaRef.current.value.substring(cursorPosition, textareaRef.current.value.length);
+        textareaRef.current.value = cursorLeft + "\t" + cursorRight;
+        textareaRef.current.selectionEnd = cursorPosition + 1;
+    }, [value, onChange]);
+
     const [markdown, setMarkdown] = useState(value);
 
     const handleChange = (e: React.ChangeEvent<HTMLTextAreaElement>) => {
@@ -24,8 +49,10 @@ const MarkdownEditor: React.FC<MarkdownEditorProps> = ({ value, onChange }: Mark
             <div>
                 <h2 className="text-lg font-semibold mb-2">編集</h2>
                 <textarea
+                    ref={textareaRef}
                     value={markdown}
                     onChange={handleChange}
+                    onKeyDown={handleKeyDown}
                     placeholder="Markdownを入力してください"
                     className="w-full h-64 p-2 border rounded resize-y"
                     spellCheck={false}
