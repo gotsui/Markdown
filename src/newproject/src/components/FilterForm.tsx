b465215c55dd689c4dd54ecba2fff3907f72256f
gotsui <krhamui.2016@gmail.com>
2025-05-27 23:16:21 +0900

    フィルターフォームの見出しを変更

--- src/newproject/src/components/FilterForm.tsx
+++ src/newproject/src/components/FilterForm.tsx
@@ -91,7 +91,7 @@ const FilterForm: React.FC<Props> = ({ isSignedIn, currentFilters }) => {
         <div className="mb-6 p-4 rounded-lg">
             <details className="border bg-gray-100 rounded-lg p-4 mb-2" open>
                 <summary className="cursor-pointer font-semibold text-lg flex items-center">
-                    <span className="mr-2">メタデータ</span>
+                    <span className="mr-2">フィルター</span>
                     <svg
                         className="w-5 h-5 transform transition-transform duration-200 [&[open]]:rotate-180"
                         fill="none"
