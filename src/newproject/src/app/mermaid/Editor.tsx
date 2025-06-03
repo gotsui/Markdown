ef80318561fd985c3e20c928bd9d055e50ffa165
gotsui <krhamui.2016@gmail.com>
2025-06-03 22:39:20 +0900

    mermaidエディタのタブ入力制御

--- src/newproject/src/app/mermaid/Editor.tsx
+++ src/newproject/src/app/mermaid/Editor.tsx
@@ -14,6 +14,91 @@ export const Editor: React.FC<EditorProps> = ({ onChange }) => {
         setCode(nextCode);
         onChange(nextCode);
     };
+    
+    const handleKeyDown = (e: React.KeyboardEvent<HTMLTextAreaElement>) => {
+        if (e.key !== "Tab") {
+            return;
+        }
+
+        // デフォルトのタブ移動をキャンセル
+        e.preventDefault();
+
+        if (e.shiftKey) {
+            const textarea = e.target as HTMLTextAreaElement;
+            const startPosition = textarea.selectionStart;
+            const endPosition = textarea.selectionEnd;
+            const value = textarea.value;
+            const startRow = value.slice(0, startPosition).split("\n").length;
+            const endRow = value.slice(startPosition, endPosition).split("\n").length + startRow - 1;
+            const rowValues = value.split("\n");
+
+            let nextStartPosition = startPosition;
+            let nextEndPosition = endPosition;
+
+            for (let i = startRow; i <= endRow; i++) {
+                const currentIndex = i - 1;
+                const currentRowValue = rowValues[currentIndex];
+
+                if (!currentRowValue.startsWith("\t")) {
+                    continue;
+                }
+
+                if (i === startRow) {
+                    nextStartPosition--;
+                }
+
+                nextEndPosition--;
+                rowValues[currentIndex] = currentRowValue.replace("\t", "");
+            }
+
+            const nextValue = rowValues.join("\n");
+            setCode(nextValue);
+
+            setTimeout(() => {
+                textarea.selectionStart = nextStartPosition;
+                textarea.selectionEnd = nextEndPosition;
+            }, 0);
+        } else {
+            const textarea = e.target as HTMLTextAreaElement;
+            const startPosition = textarea.selectionStart;
+            const endPosition = textarea.selectionEnd;
+            const value = textarea.value;
+            let nextValue = "";
+            let nextStartPosition = startPosition;
+            let nextEndPosition = endPosition;
+
+            if (startPosition === endPosition) {
+                nextValue = value.substring(0, startPosition) + "\t" + value.substring(endPosition);
+                nextStartPosition++;
+                nextEndPosition++;
+            } else {
+                const startRow = value.slice(0, startPosition).split("\n").length;
+                const endRow = value.slice(startPosition, endPosition).split("\n").length - 1 + startRow;
+
+                if (startRow === endRow) {
+                    nextValue = value.substring(0, startPosition) + "\t" + value.substring(endPosition);
+                    nextEndPosition -= (endPosition - startPosition) - 1;
+                } else {
+                    const rowValues = value.split("\n");
+                    for (let i = startRow; i <= endRow; i++) {
+                        const currentIndex = i - 1;
+                        rowValues[currentIndex] = "\t" + rowValues[currentIndex];
+                    }
+
+                    nextValue = rowValues.join("\n");
+                    nextStartPosition++;
+                    nextEndPosition += (endRow - startRow) + 1;
+                }
+            }
+
+            setCode(nextValue);
+
+            setTimeout(() => {
+                textarea.selectionStart = nextStartPosition;
+                textarea.selectionEnd = nextEndPosition;
+            }, 0);
+        }
+    };
 
     return (
         <div className="h-full p-4">
@@ -22,6 +107,8 @@ export const Editor: React.FC<EditorProps> = ({ onChange }) => {
                 className="w-full h-[calc(100%-2rem)] p-2 border rounded resize-none font-mono"
                 value={code}
                 onChange={handleChange}
+                onKeyDown={handleKeyDown}
+                spellCheck={false}
             />
         </div>
     );
