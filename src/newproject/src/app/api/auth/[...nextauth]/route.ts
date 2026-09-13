09747e5006d507a735758d78606c79ea461b3643
gotsui <krhamui.2016@gmail.com>
2026-09-13 15:14:53 +0900

    GitHub が RFC 9207に対応したことによる変更

--- src/newproject/src/app/api/auth/[...nextauth]/route.ts
+++ src/newproject/src/app/api/auth/[...nextauth]/route.ts
@@ -10,6 +10,7 @@ export const authOptions = {
         GitHubProvider({
             clientId: process.env.GITHUB_CLIENT_ID!,
             clientSecret: process.env.GITHUB_CLIENT_SECRET!,
+            issuer: "https://github.com/login/oauth",
         }),
     ],
     callbacks: {
